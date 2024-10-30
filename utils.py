911582697beaa17868191c20bf6dad222f421097
kirienko <yury.kirienko@gmail.com>
2024-10-30 10:00:39 +0100

    [skip ci] remove the `haversine_distance` function
    Use the one from the `gpxpy.geo`.

--- utils.py
+++ utils.py
@@ -47,19 +47,6 @@ def format_func(value, tick_number):
     return decimal_to_dms(value)
 
 
-def haversine_distance(lat1, lon1, lat2, lon2) -> float:
-    # Geodetic distance between two points, in km
-    R = 6371.0  # Earth radius in kilometers
-
-    dlat = math.radians(lat2 - lat1)
-    dlon = math.radians(lon2 - lon1)
-
-    a = math.sin(dlat / 2)**2 + math.cos(math.radians(lat1)) * math.cos(math.radians(lat2)) * math.sin(dlon / 2)**2
-    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
-
-    return R * c
-
-
 def km_to_nm(dist: float) -> float:
     return dist/1.852
 
