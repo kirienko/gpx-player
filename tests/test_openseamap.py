0713f26373022d46f6b791e94ad344a574347d22
kirienko <yury.kirienko@gmail.com>
2025-07-20 17:25:56 +0200

    add tests

--- tests/test_openseamap.py
+++ tests/test_openseamap.py
@@ -1,5 +1,16 @@
 import tempfile
-from openseamap import parse_gpx, speed_to_color
+import datetime as dt
+
+import pytest
+
+import gpxpy.geo
+
+from openseamap import (
+    parse_gpx,
+    speed_to_color,
+    accumulate_distances,
+    calculate_average_speeds,
+)
 
 def test_parse_gpx():
     sample_gpx = '''<?xml version="1.0" encoding="UTF-8"?>
@@ -50,3 +61,42 @@ def test_speed_to_color():
     assert isinstance(color, str)
     assert color.startswith("#")
     assert len(color) == 7
+
+
+def test_accumulate_distances_and_avg_speed():
+    points = [
+        {
+            'lat': 0.0,
+            'lon': 0.0,
+            'time': dt.datetime(2020, 1, 1, 0, 0, 0, tzinfo=dt.timezone.utc),
+        },
+        {
+            'lat': 0.0,
+            'lon': 1.0,
+            'time': dt.datetime(2020, 1, 1, 1, 0, 0, tzinfo=dt.timezone.utc),
+        },
+        {
+            'lat': 0.0,
+            'lon': 2.0,
+            'time': dt.datetime(2020, 1, 1, 2, 0, 0, tzinfo=dt.timezone.utc),
+        },
+    ]
+
+    dists = accumulate_distances(points)
+
+    expected_first = 0.0
+    one_deg_nm = gpxpy.geo.haversine_distance(0, 0, 0, 1) / 1852.0
+    expected_second = one_deg_nm
+    expected_third = 2 * one_deg_nm
+
+    assert len(dists) == 3
+    assert dists[0] == pytest.approx(expected_first)
+    assert dists[1] == pytest.approx(expected_second)
+    assert dists[2] == pytest.approx(expected_third)
+
+    avgs = calculate_average_speeds(points, dists)
+
+    assert len(avgs) == 3
+    assert avgs[0] == 0.0
+    assert avgs[1] == pytest.approx(expected_second / 1.0)
+    assert avgs[2] == pytest.approx(expected_third / 2.0)
